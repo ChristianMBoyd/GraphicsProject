9862beaa105b3ccffb43b4055c73c7245fd0e3ec
Christian Boyd <boyd.christian.m@gmail.com>
2024-03-31 19:13:00 -0600

    OmegaException copied from Youtube video as Exception. Should consider implementing Modules

--- GraphicsProjectSolution/GraphicsProject/Exception.hpp
+++ GraphicsProjectSolution/GraphicsProject/Exception.hpp
@@ -1,4 +1,4 @@
-#pragma once
+// std
 #include <string>
 #include <source_location>
 #include <exception>
@@ -8,12 +8,14 @@ template<typename DATA>
 class Exception : std::exception
 {
 public:
-//	Exception()
+	Exception(std::string description, DATA data, const std::source_location& location = std::source_location::current(), std::stacktrace stacktrace = std::stacktrace::current()) :
+		description_{ std::move(description) }, data_{ std::move(data) }, location_{ location }, stacktrace_{ stacktrace }
+	{}
 
 
 
 private:
-	std::string err_string_;
+	std::string description_;
 	DATA data_;
 	const std::source_location location_;
 	const std::stacktrace stacktrace_;
